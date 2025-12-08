749b3ddccc762853348f2dc00b5c095e482b6a70
Muhammad Shahzaib <shahzaibr308@gmail.com>
2025-12-08 12:21:50 +0500

    Latest code

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
-
 // model 
 const UserModel = require('./Models/UserModel');
 const ExpenseModel = require('./Models/ExpenseModel');
-
-// database 
+// database np
 const db = require('./Config/connection-mongoose');
-
 // router 
 const userRouter = require('./Routers/userRouter');
 const ExpenseRouter = require('./Routers/ExpenseRouter');
-
 app.use(express.json());                    
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use('/user', userRouter);
 app.use('/expense', ExpenseRouter);
-
-
 app.listen(3000, ()=>{
-    console.log('server is running');
-    
+    console.log('server is running');  
 });
